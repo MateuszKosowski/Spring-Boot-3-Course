a275af83685161b45c16d3e3a35b8cd1fdea8301
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2026-05-05 22:13:03 +0200

    Rename folders + JdbcUserDetailsManager

---
+++ REST API Security ch.5/security_app/src/main/java/com/kosowski/securitydemo/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.kosowski.securitydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        // {noop} means that the password is stored in plain text, without any encoding,
+//        // There are other options for password encoding, such as argon2
+//        manager.createUser(User.withUsername("admin").password("{noop}admin").roles("ADMIN").build());
+//        manager.createUser(User.withUsername("user").password("{noop}user").roles("USER").build());
+//        manager.createUser(User.withUsername("viewer").password("{noop}viewer").roles("VIEWER").build());
+//        return manager;
+//    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    SecurityFilterChain securityWebFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests(
+                config -> config
+                        .requestMatchers(HttpMethod.GET,"/api/v1/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/v1/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/v1/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/v1/employees/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH,"/api/v1/employees/**").hasRole("MANAGER")
+                        .anyRequest().authenticated()
+        );
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.httpBasic(Customizer.withDefaults()).build();
+    }
+
+
+
+
+}
