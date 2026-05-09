f0b42c69bfeb15ead76306764d4a1fb79e4e95e5
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2026-05-09 17:00:51 +0200

    Add form handling with Thymeleaf for user input and response display

--- Spring MVC ch.6/thymeleaf/src/main/java/com/kosowski/thymeleaf/controller/DemoController.java
+++ Spring MVC ch.6/thymeleaf/src/main/java/com/kosowski/thymeleaf/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.kosowski.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 
@@ -13,5 +15,16 @@ public class DemoController {
     public String sayHello(Model theModel) {
         theModel.addAttribute("theDate", LocalDateTime.now());
         return "hello";
+    }
+
+    @GetMapping("/form")
+    public String showForm(Model theModel) {
+        return "form";
+    }
+
+    @PostMapping("/form-response")
+    public String formResponse(Model theModel, @RequestParam("name") String name) {
+        theModel.addAttribute("name_model", name);
+        return "form-response";
     }
 }
